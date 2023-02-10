b568be46e3195402329796398ee2e39af803b0cc
Boris Lanovenko <blanovenko@gmail.com>
2023-02-10 17:46:42 +0300

    Create Project 4

---
+++ Project_4/Program.cs
@@ -0,0 +1,7 @@
+﻿double numA = new Random().Next(1,20);
+Console.WriteLine(numA);
+double numB = new Random().Next(1,20);
+Console.WriteLine(numB);
+double result = numA / numB;
+Console.Write("Result=");
+Console.WriteLine(result);
