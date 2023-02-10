866144830c7d950432f1b0980601990b43993ca3
Boris Lanovenko <blanovenko@gmail.com>
2023-02-10 18:01:56 +0300

    Create Project 5

---
+++ Project_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name:");
+string user = Console.ReadLine();
+
+if (user.ToLower() == "boris")
+{
+  Console.WriteLine("Ole, it's Boris!!!");
+}
+else
+{
+  Console.WriteLine("Hi, ");
+  Console.WriteLine(user);
+}
