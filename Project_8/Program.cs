9ce5f8c5ad93f1a119ad255b531cc6817b087feb
Boris Lanovenko <blanovenko@gmail.com>
2023-02-11 16:18:57 +0300

    Create project 8 DogRun

--- Project_8/Program.cs
+++ Project_8/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача с собакой бегующей от одного друга к другому.
+
+Console.Clear();
+Console.Write("Введите расстояние между друзьми: ");
+int dist = Convert.ToInt32(Console.ReadLine());
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (dist > 10)
+{
+  if (friend == 1)
+  {
+    time = dist / (firstFriendSpeed + dogSpeed);
+    friend = 2;
+  }
+  else
+  {
+    time = dist / (secondFriendSpeed + dogSpeed);
+    friend = 1;
+  }
+  dist = dist - (firstFriendSpeed + secondFriendSpeed) * time;
+  count = count + 1;
+}
+Console.WriteLine("Собака пробежит " + count + " раз.");
